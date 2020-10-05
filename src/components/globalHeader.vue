0d2bf3c85dc2ef9f809aeb4269448c38d9f5b479
simari1 <bdfsg610@yahoo.co.jp>
2020-10-06 01:36:44 +0900

    router link に変更

--- src/components/globalHeader.vue
+++ src/components/globalHeader.vue
@@ -3,12 +3,14 @@
     <div>
       <b-nav class="bg-dark">
         <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
-          <b-nav-item href="/"><span class="text-white">Home</span></b-nav-item>
-          <b-nav-item href="/Skills"
-            ><span class="text-white">Skills</span></b-nav-item
+          <router-link class="nav-link px-4 py-3" to="/"
+            ><span class="text-white">Home</span></router-link
           >
-          <b-nav-item href="/Experience"
-            ><span class="text-white">Experience</span></b-nav-item
+          <router-link class="nav-link px-4 py-3" to="/Skills"
+            ><span class="text-white">Skills</span></router-link
+          >
+          <router-link class="nav-link px-4 py-3" to="/Experience"
+            ><span class="text-white">Experience</span></router-link
           >
         </div>
       </b-nav>
